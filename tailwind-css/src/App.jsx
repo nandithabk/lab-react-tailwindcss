dfb65bc3ba4d615c59917f55c5fbefb1d0e4209e
Nanditha <bknanditha.2020@gmail.com>
2024-02-24 11:29:25 +0530

    updated

---
+++ tailwind-css/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Alert from "./assets/components/Alert";
+import Button from "./assets/components/Button";
+import Card from "./assets/components/Card";
+import Footer from "./assets/components/Footer";
+import Navbar from "./assets/components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Button />
+      <Alert />
+      <Card />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
